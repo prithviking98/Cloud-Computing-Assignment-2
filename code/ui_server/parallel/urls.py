44a4369825821b5a6b470f55f3a3a6939731287b
Prithvi Raj Nair <prithviking98@gmail.com>
2019-11-04 15:47:08 +0530

    small chane

--- code/ui_server/parallel/urls.py
+++ code/ui_server/parallel/urls.py
@@ -5,12 +5,10 @@ from parallel import views
 app_name = 'parallel'
 
 urlpatterns = [
+	path('', views.home_html, name='home'),
 	path('write/', views.write_handler, name='write_handler'),
 	path('read/', views.read_handler, name='read_handler'),
 	path('del/',views.del_handler,name='del_handler'),
-	path('', views.home_html, name='home'),
 	path('read_html/', views.read_html, name='read_html'),
 	path('write_html/', views.write_html, name='write_html'),
-
-
 ]
